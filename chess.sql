04ab9fee4636423813dc4e59a986694e7f3a966a
tsarut <tsarut@hotmail.com>
2016-05-14 14:54:35 +0700

    update sql

--- chess.sql
+++ chess.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 13, 2016 at 09:42 AM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `chess`
 --
@@ -52,8 +34,8 @@ INSERT INTO `member` (`member_id`, `user`, `pass`, `email`) VALUES
 --
 
 CREATE TABLE `playquiz` (
-  `QUIZ_quiz_id` int(11) NOT NULL,
-  `MEMBER_member_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
   `time` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -67,7 +49,7 @@ CREATE TABLE `progress` (
   `learn_CH` binary(8) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0',
   `learn_TH` binary(8) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0',
   `learn_IN` binary(7) NOT NULL DEFAULT '\0\0\0\0\0\0\0',
-  `MEMBER_member_id` int(11) NOT NULL
+  `member_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -82,7 +64,7 @@ CREATE TABLE `quiz` (
   `quizDetail` varchar(150) DEFAULT NULL,
   `rankQuiz` int(11) NOT NULL,
   `quizDir` varchar(150) NOT NULL,
-  `MEMBER_member_id` int(11) NOT NULL
+  `member_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -96,7 +78,7 @@ CREATE TABLE `quizask` (
   `TH` int(11) NOT NULL DEFAULT '0',
   `IN` int(11) NOT NULL DEFAULT '0',
   `ALL` int(11) NOT NULL DEFAULT '0',
-  `MEMBER_member_id` int(11) NOT NULL
+  `member_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -109,7 +91,7 @@ CREATE TABLE `rank` (
   `quiz_CH` int(11) NOT NULL DEFAULT '0',
   `quiz_TH` int(11) NOT NULL DEFAULT '0',
   `quiz_IN` int(11) NOT NULL DEFAULT '0',
-  `MEMBER_member_id` int(11) NOT NULL
+  `member_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -126,34 +108,34 @@ ALTER TABLE `member`
 -- Indexes for table `playquiz`
 --
 ALTER TABLE `playquiz`
-  ADD KEY `fk_PLAYQUIZ_QUIZ1_idx` (`QUIZ_quiz_id`),
-  ADD KEY `fk_PLAYQUIZ_MEMBER1_idx` (`MEMBER_member_id`);
+  ADD KEY `fk_PLAYQUIZ_QUIZ1_idx` (`quiz_id`),
+  ADD KEY `fk_PLAYQUIZ_MEMBER1_idx` (`member_id`);
 
 --
 -- Indexes for table `progress`
 --
 ALTER TABLE `progress`
-  ADD PRIMARY KEY (`MEMBER_member_id`);
+  ADD PRIMARY KEY (`member_id`);
 
 --
 -- Indexes for table `quiz`
 --
 ALTER TABLE `quiz`
   ADD PRIMARY KEY (`quiz_id`),
-  ADD KEY `fk_QUIZ_MEMBER1_idx` (`MEMBER_member_id`);
+  ADD KEY `fk_QUIZ_MEMBER1_idx` (`member_id`);
 
 --
 -- Indexes for table `quizask`
 --
 ALTER TABLE `quizask`
-  ADD PRIMARY KEY (`MEMBER_member_id`);
+  ADD PRIMARY KEY (`member_id`);
 
 --
 -- Indexes for table `rank`
 --
 ALTER TABLE `rank`
-  ADD PRIMARY KEY (`MEMBER_member_id`),
-  ADD KEY `fk_RANK_MEMBER1_idx` (`MEMBER_member_id`);
+  ADD PRIMARY KEY (`member_id`),
+  ADD KEY `fk_RANK_MEMBER1_idx` (`member_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -177,32 +159,32 @@ ALTER TABLE `quiz`
 -- Constraints for table `playquiz`
 --
 ALTER TABLE `playquiz`
-  ADD CONSTRAINT `fk_PLAYQUIZ_MEMBER1` FOREIGN KEY (`MEMBER_member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_PLAYQUIZ_QUIZ1` FOREIGN KEY (`QUIZ_quiz_id`) REFERENCES `quiz` (`quiz_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_PLAYQUIZ_MEMBER1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_PLAYQUIZ_QUIZ1` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`quiz_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `progress`
 --
 ALTER TABLE `progress`
-  ADD CONSTRAINT `fk_PROGRESS_MEMBER1` FOREIGN KEY (`MEMBER_member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_PROGRESS_MEMBER1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `quiz`
 --
 ALTER TABLE `quiz`
-  ADD CONSTRAINT `fk_QUIZ_MEMBER1` FOREIGN KEY (`MEMBER_member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_QUIZ_MEMBER1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `quizask`
 --
 ALTER TABLE `quizask`
-  ADD CONSTRAINT `fk_QUIZSE_MEMBER1` FOREIGN KEY (`MEMBER_member_id`) REFERENCES `member` (`member_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_QUIZSE_MEMBER1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `rank`
 --
 ALTER TABLE `rank`
-  ADD CONSTRAINT `fk_RANK_MEMBER1` FOREIGN KEY (`MEMBER_member_id`) REFERENCES `member` (`member_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_RANK_MEMBER1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
