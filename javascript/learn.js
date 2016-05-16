ebc3abc033a35bf4c28f0346be1fd331c44a93da
tsarut <tsarut@hotmail.com>
2016-05-16 18:38:21 +0700

    learn เพื่มส่วนที่ตกหล่น

--- javascript/learn.js
+++ javascript/learn.js
@@ -47,6 +47,25 @@ function save(mark) {
 		}
 	}
 }
+var ch_r;
+var ch_kn;
+var ch_e;
+var ch_g;
+var ch_k;
+var ch_c;
+var ch_p;
+var th_r=[2, 7, 10, 11, 13, 14, 17, 22];
+var th_kn=[1, 3, 5, 9, 15, 19, 21, 23];
+var th_b=[6, 7, 8, 16, 18];
+var th_k=[6, 7, 8, 11, 13, 16, 17, 18];
+var th_q=[6, 8, 16, 18];
+var th_p=[6, 8, 16, 17, 18];
+var in_r=[2, 8, 11, 12, 14, 15, 18, 23];
+var in_kn=[1, 3, 6, 10, 16, 20, 22, 24];
+var in_b=[0, 4, 7, 9, 17, 19, 21, 25];
+var in_k=[7, 8, 9, 12, 14, 17, 18, 19];
+var in_q=[2, 7, 8, 9, 11, 12, 14, 15, 17, 18, 19, 23];
+var in_p=[18];
 function getSubmit(name) {
 	// body...
 	var sum=[];
@@ -58,6 +77,32 @@ function getSubmit(name) {
 			sum.push(i);
 		}
 	}
+	var groupNum=["ch_r","ch_kn","ch_e","ch_g","ch_k","ch_c","ch_p","th_r","th_kn","th_b","th_k","th_q","th_p","in_r","in_kn","in_b","in_k","in_q","in_p"];
+	var groupTest=[ch_r,ch_kn,ch_e,ch_g,ch_k,ch_c,ch_p,th_r,th_kn,th_b,th_k,th_q,th_p,in_r,in_kn,in_b,in_k,in_q,in_p];
+	var test=0;
+	for (var i = 0; i < groupNum.length; i++) {
+		if (groupNum[i]==name) {
+			test=i;
+		}
+	}
+	if (groupTest[test].length!=sum.length) {
+		alert('not correct')
+	} else {
+		var correct=1;
+		for (var i = 0; i < sum.length; i++) {
+			if (sum[i]!=groupTest[test][i]) {
+				correct=0;
+			}
+		}
+		alert('correct');
+		if (i<=6) {
+		window.parent.location.href='addPro.php?name=learn_CH&num='+i;	
+		} else {
+			if (i<=12) {
+				window.parent.location.href='addPro.php?name=learn_TH&num='+i-6;
+			} else {window.parent.location.href='addPro.php?name=learn_TH&num='+i-12}
+		}
+		window.parent.location.href='addPro.php?name='name;
+	}
 	console.log(sum);
-	return sum;
 }
