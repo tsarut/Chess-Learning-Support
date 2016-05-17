4a30a74338993145a011acc9c2f3cc8bfaf1caeb
tsarut <tsarut@hotmail.com>
2016-05-18 02:15:03 +0700

    move php to DB
    ไฟล์นี้ไว้ใช้เรียกข้อมูลเข้า DB ของ

---
+++ learn/test/addPro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+	session_start();
+	echo $_GET['name'];
+	echo $_GET['num'];
+	mysql_connect("localhost","root","");
+	mysql_select_db("chess");
+	$strSQL = "SELECT * FROM progress WHERE member_id ='".$_SESSION["UserID"]."'";
+	$objQuery = mysql_query($strSQL);
+	$objResult = mysql_fetch_array($objQuery);
+	$arr = str_split($objResult[$_GET['name']]);
+	$arr[$_GET['num']] = '1';
+	$str = implode('',$arr);
+	$strSQL = "UPDATE progress SET ".$_GET['name']." ='".$str."'  WHERE member_id = '".$_SESSION["UserID"]."' ";
+	$objQuery = mysql_query($strSQL);
+	//$strSQL = "UPDATE progress SET ".$_GET['name']." =".$_GET['num']."  WHERE UserID = '".$_SESSION["UserID"]."' ";
+
+ ?>
+
+</body>
+</html>
